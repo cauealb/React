be531d129cf982e2e5154c3307d7cd5345ef26cc
Cauê Alves <cauealvesb4@gmail.com>
2024-12-05 20:53:21 -0300

    Finalizando o Componente do Header

--- 01-fundamentos/src/Components/Header.jsx
+++ 01-fundamentos/src/Components/Header.jsx
@@ -1,9 +1,11 @@
 import head from './Header.module.css'
-console.log(head)
+import img from '../assets/ignite-logo.svg'
+console.log(img)
 
 export function Header() {
     return (
         <header className={head.header}>
+            <img src={img} alt="Logo da Ignite" />
             <h1>Ignite Feed</h1>
         </header>
     )
