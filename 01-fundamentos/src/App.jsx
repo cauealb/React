a6b7c1742cc7b91e79404cdce083084921af649f
Cauê Alves <cauealvesb4@gmail.com>
2024-12-05 21:19:01 -0300

    Começando o componente SideBar

--- 01-fundamentos/src/App.jsx
+++ 01-fundamentos/src/App.jsx
@@ -1,5 +1,7 @@
 import { Header } from "./Components/Header"
 import { Post } from "./Components/Post"
+import AppCSS from './Components/App.module.css'
+import { SideBar } from "./Components/Sidebar"
 
 export function App() {
 
@@ -7,15 +9,20 @@ export function App() {
     <div>
       <Header />
 
-      <Post
-      autor="Cauê"
-      content="Um post sobre React"
-    />
+      <div className={AppCSS.wapper}>
+        <SideBar />
+        <main>
+        <Post
+          autor="Cauê"
+          content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque amet nisi aut corporis, quaerat distinctio neque architecto. Aliquam magnam culpa dolorum deserunt laudantium! Soluta odio veritatis sequi repudiandae ab reiciendis? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque amet nisi aut corporis, quaerat distinctio neque architecto. Aliquam magnam culpa dolorum deserunt laudantium! Soluta odio veritatis sequi repudiandae ab reiciendis?"
+        />
 
-    <Post
-      autor="Gabriel"
-      content="Um post sobre Angular"
-    />
+        <Post
+          autor="Gabriel"
+          content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque amet nisi aut corporis, quaerat distinctio neque architecto. Aliquam magnam culpa dolorum deserunt laudantium! Soluta odio veritatis sequi repudiandae ab reiciendis?"
+        />
+        </main>
+      </div>
     </div>
   )
 }
---
+++ 01-fundamentos/src/Components/Sidebar.jsx
@@ -0,0 +1,9 @@
+import sidebar from './Siderbar.module.css'
+
+export function SideBar() {
+    return (
+        <aside className={sidebar.side}>
+            side
+        </aside>
+    );
+};
