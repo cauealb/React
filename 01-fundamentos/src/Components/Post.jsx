53845fbf2eb3ad854fcf8c93ad9177b3ad155784
Cauê Alves <cauealvesb4@gmail.com>
2024-12-07 09:13:37 -0300

    Finalizando o conteúdo do post

--- 01-fundamentos/src/Components/Post.jsx
+++ 01-fundamentos/src/Components/Post.jsx
@@ -7,18 +7,20 @@ export function Post(){
                 <div className={post.author}>
                 <img 
                     className={post.img}
-                    src="https://avatars.githubusercontent.com/u/167707073?v=4" 
-                />
+                    src="https://avatars.githubusercontent.com/u/167707073?v=4" />
                     <div className={post.head}>
                         <strong>Cauê Alves</strong>
                         <span>UI Design</span>
                     </div>
                 </div>
-                <time>Pública há 1hr</time>
+                <time title='07 de dezembro de 2024'>Pública há 1hr</time>
             </header>
 
             <div className={post.infos}>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis non quae accusantium minima! Eligendi et at vel facilis inventore molestiae explicabo, ad placeat cumque? Dignissimos voluptates deleniti officiis in architecto?</p>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p>👉 <a href='#'>jane.design/doctorcare</a></p>
+                <p><a href='#'>#novoprojeto #nlw #rocketseat</a></p>
             </div>
         </section>
     );
