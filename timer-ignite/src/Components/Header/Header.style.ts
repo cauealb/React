26dede591b211fc93be24f0943f7b44c24128c42
Cauê Alves <cauealvesb4@gmail.com>
2024-12-15 19:43:59 -0300

    Criando Estilização para os icons

--- timer-ignite/src/Components/Header/Header.style.ts
+++ timer-ignite/src/Components/Header/Header.style.ts
@@ -9,4 +9,26 @@ export const LayoutHeader = styled.header`
     img {
         height: 7px
     }
+
+    nav {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 0.45rem;
+    }
+
+    a {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 0.75rem;
+        height: 0.75rem;
+        color: ${props => props.theme['--gray-100']};
+        border-bottom: 0.5px solid transparent;
+        border-top: 0.5px solid transparent;
+
+        &:hover {
+            border-bottom: 0.5px solid ${props => props.theme['--green-500']}
+        }
+    }
 `;
