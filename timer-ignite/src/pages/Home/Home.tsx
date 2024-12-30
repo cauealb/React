01db5ee979f5f17cb2fde420b2d0e7256099a480
Cauê Alves <cauealvesb4@gmail.com>
2024-12-29 21:34:49 -0300

    Colocando o react hook form

--- timer-ignite/src/pages/Home/Home.tsx
+++ timer-ignite/src/pages/Home/Home.tsx
@@ -1,13 +1,26 @@
 import { Play } from "phosphor-react";
 import { PrincipalStyled, InputsStyled, StyleDurantion, StyledButton, Separator, StyledSpan, TaskInput, MinuteInput } from "./Home.style";
+import { useForm } from 'react-hook-form'
 
 export function Home() {
+
+    const {register, handleSubmit, watch} = useForm()
+
+    function handleNewCreteTask(data: any){
+        console.log(data)
+    }
+
+    const task = watch('task')
+    const minute = watch('minute')
+    const isSubmitInvalid = !task || !minute
+
     return (
             <>
-                <PrincipalStyled>
+                <PrincipalStyled onSubmit={handleSubmit(handleNewCreteTask)}>
                     <InputsStyled>
                         <label htmlFor="taks">Vou trabalhar em</label>
                         <TaskInput  
+                            {...register('task')}
                             type="text" 
                             id="taks"  
                             list="DataList"
@@ -31,6 +44,7 @@ export function Home() {
                             step={5}
                             min={5}
                             max={60}
+                            {...register('minute', { valueAsNumber: true })}
                         />
 
                         <span>minutos.</span>
@@ -44,7 +58,7 @@ export function Home() {
                         <StyledSpan>0</StyledSpan>
                     </StyleDurantion>
 
-                    <StyledButton type="submit" disabled>
+                    <StyledButton type="submit" disabled={isSubmitInvalid}>
                         <Play size={24}/>
                         Começar
                     </StyledButton>
